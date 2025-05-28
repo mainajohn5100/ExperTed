7d9b892660d50e5ae2f83a6af5e7a175219e4f64
maina john <mainajohn5100@gmail.com>
2025-05-28 02:29:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <tr>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <ErrorBoundary errorComponent={undefined} errorStyles={undefined} errorScripts={undefined}>
    >       <LoadingBoundary loading={null}>
    >         <HTTPAccessFallbackBoundary notFound={undefined} forbidden={undefined} unauthorized={undefined}>
    >           <RedirectBoundary>
    >             <RedirectErrorBoundary router={{...}}>
    >               <InnerLayoutRouter url="/dashboard" tree={[...]} cacheNode={{lazyData:null, ...}} segmentPath={[...]}>
    >                 <DashboardPage>
    >                   <Lazy>
    >                   <div className="flex flex-...">
    >                     <div>
    >                     <Lazy>
    >                     <RecentTicketsTable>
    >                       <Card>
    >                         <div className="rounded-lg...">
    >                           <div>
    >                           <CardContent>
    >                             <div className="p-6 pt-0">
    >                               <Table>
    >                                 <div className="relative w...">
    >                                   <table className="w-full cap...">
    >                                     <thead>
    >                                     <TableBody>
    >                                       <tbody className={"[&_tr:la..."}>
    >                                         <TableRow>
    > >                                         <tr
    > >                                           className="border-b transition-colors hover:bg-muted/50 data-[state=select..."
    > >                                         >
    > >                                           {" "}
    >                                             ...
    >                 ...
    >
    >
    > src/components/ui/table.tsx (66:3) @ TableRow
    >
    >   64 |   React.HTMLAttributes<HTMLTableRowElement>
    >   65 | >(({ className, children, ...props }, ref) => (
    > > 66 |   <tr
    >      |   ^
    >   67 |     ref={ref}
    >   68 |     className={cn(
    >   69 |       "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
    >
    > Call Stack
    > 18
    >
    > Show 13 ignore-listed frame(s)
    > tr
    > <anonymous> (0:0)
    > TableRow
    > src/components/ui/table.tsx (66:3)
    > <anonymous>
    > src/components/dashboard/recent-tickets-table.tsx (63:15)
    > RecentTicketsTable
    > src/components/dashboard/recent-tickets-table.tsx (62:28)
    > DashboardPage
    > src/app/(app)/dashboard/page.tsx (72:7)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -5,15 +6,13 @@ import { cn } from "@/lib/utils"
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
->(({ className, children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
-      {...props}
-    >
-      {children}
-    </table>
+      {...props} // children will be passed via props
+    />
   </div>
 ))
 Table.displayName = "Table"
@@ -21,31 +20,27 @@ Table.displayName = "Table"
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, children, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props}>
-    {children}
-  </thead>
+>(({ className, ...props }, ref) => (
+  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
 ))
 TableHeader.displayName = "TableHeader"
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
     className={cn("[&_tr:last-child]:border-0", className)}
     {...props}
-  >
-    {children}
-  </tbody>
+  />
 ))
 TableBody.displayName = "TableBody"
 
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <tfoot
     ref={ref}
     className={cn(
@@ -53,16 +48,14 @@ const TableFooter = React.forwardRef<
       className
     )}
     {...props}
-  >
-    {children}
-  </tfoot>
+  />
 ))
 TableFooter.displayName = "TableFooter"
 
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <tr
     ref={ref}
     className={cn(
@@ -70,16 +63,14 @@ const TableRow = React.forwardRef<
       className
     )}
     {...props}
-  >
-    {children}
-  </tr>
+  />
 ))
 TableRow.displayName = "TableRow"
 
 const TableHead = React.forwardRef<
   HTMLTableCellElement,
   React.ThHTMLAttributes<HTMLTableCellElement>
->(({ className, children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <th
     ref={ref}
     className={cn(
@@ -87,37 +78,31 @@ const TableHead = React.forwardRef<
       className
     )}
     {...props}
-  >
-    {children}
-  </th>
+  />
 ))
 TableHead.displayName = "TableHead"
 
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
   React.TdHTMLAttributes<HTMLTableCellElement>
->(({ className, children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <td
     ref={ref}
     className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
     {...props}
-  >
-    {children}
-  </td>
+  />
 ))
 TableCell.displayName = "TableCell"
 
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
   React.HTMLAttributes<HTMLTableCaptionElement>
->(({ className, children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <caption
     ref={ref}
     className={cn("mt-4 text-sm text-muted-foreground", className)}
     {...props}
-  >
-    {children}
-  </caption>
+  />
 ))
 TableCaption.displayName = "TableCaption"
 
